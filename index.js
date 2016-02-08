c48c9257965b9d8f49be8f64b6e5544938e3d722
h3x3d <h3x3d@ya.ru>
2016-02-08 03:52:59 +0300

    Improvements

--- index.js
+++ index.js
@@ -1,40 +1,9 @@
 module.exports = {
+  'extends': 'airbnb/base',
   'parser': 'babel-eslint',
-  'ecmaFeatures':  {
-    'arrowFunctions': true,
-    'blockBindings': true,
-    'classes': true,
-    'defaultParams': true,
-    'destructuring': true,
-    'forOf': true,
-    'generators': true,
-    'modules': true,
-    'objectLiteralComputedProperties': true,
-    'objectLiteralDuplicateProperties': false,
-    'objectLiteralShorthandMethods': true,
-    'objectLiteralShorthandProperties': true,
-    'restParams': true,
-    'spread': true,
-    'superInFunctions': true,
-    'templateStrings': true,
-    'jsx': false
-  },
   'rules': {
-    'arrow-body-style': [2, 'as-needed'],
-    'arrow-parens': 0,
-    'arrow-spacing': [2, { 'before': true, 'after': true }],
-    'constructor-super': 0,
-    'generator-star-spacing': 0,
-    'no-class-assign': 0,
-    'no-const-assign': 2,
-    'no-this-before-super': 0,
-    'no-var': 2,
-    'object-shorthand': [2, 'always'],
-    'prefer-arrow-callback': 2,
-    'prefer-const': 2,
-    'prefer-spread': 0,
-    'prefer-reflect': 0,
-    'prefer-template': 2,
-    'require-yield': 0
+    'no-shadow': [0],
+    'func-names': [0],
+    'no-param-reassign': [0]
   }
 };
