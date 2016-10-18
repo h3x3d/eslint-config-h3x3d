0469f5ee3578d2e36e81997d1f83654ede4df8a2
h3x3d <h3x3d@ya.ru>
2016-10-18 12:14:25 +0300

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports = {
-  'extends': 'airbnb/base',
-  'parser': 'babel-eslint',
-  'plugins': ['babel'],
+  'plugins': ['react'],
+  'extends': 'airbnb',
+  'parserOptions': {
+    "ecmaVersion": 8
+  },
   'rules': {
     'no-shadow': [0],
     'func-names': [0],
@@ -10,9 +12,6 @@ module.exports = {
     'new-cap': [0],
     'padded-blocks': [0],
     'no-plusplus': [0],
-    'arrow-parens': 0,
-    'babel/arrow-parens': [2, 'as-needed'],
-    'generator-star-spacing': 0,
-    'babel/generator-star-spacing': [2, { 'before': false, 'after': true }]
+    'arrow-parens': 0
   }
 };
